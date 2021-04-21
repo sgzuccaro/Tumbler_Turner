800427bb3a81052c6cd8e167083d8e6293952ce6
sgzuccaro <72237672+sgzuccaro@users.noreply.github.com>
2021-04-21 17:37:15 -0500

    first working version v1

--- tumbler_firmware/src/main.cpp
+++ tumbler_firmware/src/main.cpp
@@ -8,12 +8,16 @@
 #define ENABLE_PIN 4 //D4
 #define DEBUG_LED 13 
 
+#define MS1_PIN 9 
+#define MS2_PIN 10 
+#define MS3_PIN 11 
+
 #define POT_PIN A0 
 
 AccelStepper stepper(AccelStepper::DRIVER,STEP_PIN,DIR_PIN); //DRIVER means your using a stepper driver
-double max_rps=0.2; //12rpm/60
+double max_rps=0.13; //8rpm/60
 double max_rpss=25;
-double dead_zone=10; //rps
+double dead_zone=20; //rps
 double steps_per_rev=3200; //200*microsteps
 
 Metro pot_check=Metro(100); //milliseconds 0.1 sec
@@ -21,20 +25,25 @@ Metro pot_check=Metro(100); //milliseconds 0.1 sec
 double max_vel;
 double max_accel;
 double vel=0;
+double vel_offset=0;
 
-int pot_dead_zone=2;
+int pot_dead_zone=2; // change detected size
 int pot=0;
 int prev_pot=0;
 
-
+bool enabled_state=false;
 
 
 void setup(){
-
-  // serial communication
-  Serial.begin(9600);
+ 
 
   pinMode(DEBUG_LED, OUTPUT);
+  pinMode(MS1_PIN, OUTPUT);
+  pinMode(MS2_PIN, OUTPUT);
+  pinMode(MS3_PIN, OUTPUT);
+  digitalWrite(MS1_PIN, HIGH);
+  digitalWrite(MS2_PIN, HIGH);
+  digitalWrite(MS3_PIN, HIGH);
 
   //convert rps to steps/s
   max_vel=max_rps*steps_per_rev;
@@ -49,14 +58,9 @@ void setup(){
   stepper.setEnablePin(ENABLE_PIN); // GRD=ON VIO=OFF
 
   stepper.enableOutputs(); // setspin modes as outputs
-
-  for(uint8_t i=0; i<6; i++){
-    digitalWrite(DEBUG_LED, HIGH);
-    delay(100);
-    digitalWrite(DEBUG_LED, LOW);
-    delay(100);
-  }
-  Serial.println("Setup complete");
+  enabled_state=true;
+   
+ 
 }
 
 
@@ -74,22 +78,23 @@ void loop(){
 
       prev_pot=pot;
 
-      //map pot to speed
+       //map pot to speed
       vel = map(pot, 0, 1023, -max_vel, max_vel);
 
       //if near zero
-      if(abs(vel)<dead_zone){vel=0;}
+      if(abs(vel)<dead_zone){
+        vel=0;
+      
+      }
 
-      //dir based on vel sign
       stepper.setSpeed(vel);
 
-      Serial.println(vel);
+    } // if pot changed
 
-      } 
+  }//if pot read timer
 
-
-    }//if pot read timer
 
   stepper.runSpeed(); //call as often as possible. This steps when needed for speed control
+  
 
 }
